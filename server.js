b2750c83e1722ed4ee3a562a9d23d9b87dc79eaf
alphab1t <alphabit@outlook.com>
2013-09-11 20:31:51 -0700

    working...

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 var http = require('http');
+var url = require("url");
 
-function start() {
+function start(route) {
 	function onRequest(request, response) {
 		console.log("request received"); 
+		pathname = url.parse(request.url).pathname;
 		route(pathname);
-		res.writeHead(200, {'Content-Type': 'text/plain'});
-		res.end('Hello World\n');
+		response.writeHead(200, {'Content-Type': 'text/plain'});
+		response.end('Hello World\n');
 	}
 	
 	http.createServer(onRequest).listen(1337, '127.0.0.1');
